70268232916e17dea7c80a54e9e302a7a20c57ae
Xavier Savoie <xaviersavoie@gmail.com>
2021-09-22 17:40:07 +0000

    created input.js and moved stdin functionin that file

--- play.js
+++ play.js
@@ -1,26 +1,9 @@
 const net = require("net");
-const connect = require("./client")
+const { connect } = require("./client")
+const { setupInput } = require("./input")
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  const handleUserInput = function (key) {
-    if (key === '\u0003') {
-      process.exit()
-    }
-  };
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-setupInput()
 
 console.log("Connecting ...");
 connect();
+
+setupInput()
